e06526816ed362f54e1963d528f611ebaa04a055
“[Nicol <[nicol.saha@gmail.com]>
2020-09-11 13:51:18 +0200

    Step 1+2 Finished, working on products bug

---
+++ form-view.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
+          rel="stylesheet"/>
+    <title>Order food & drinks</title>
+</head>
+<body>
+<div class="container">
+    <h1>Order food in restaurant "the Personal Ham Processors"</h1>
+    <nav>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="?food=1">Order food</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?food=0">Order drinks</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="alert alert-success" role="alert" <?php echo $style_success ?>> <?php echo $thankyou_message ?> </div>
+    <form method="post" action="index.php">
+        <div class="form-row">
+    
+            <div class="form-group col-md-6">
+                <div class="alert alert-danger" id="email-alert" role="alert" <?php echo $style_warning_email?> > <?php echo $error_message_email; ?> </div>
+                <div class="alert alert-success" id="email-alert" role="alert" <?php echo $style_success_email?> > <?php echo $success_message_email; ?> </div>
+                <label for="email">E-mail:</label>
+                <input type="text" id="email" name="email" class="form-control" value="<?php echo $email;  ?>" required/> 
+            </div>
+            <div></div>
+        </div>
+
+        <fieldset>
+            <legend>Address</legend>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                <div class="alert alert-danger" id="street-alert" role="alert" <?php echo $style_warning_street?> > <?php echo $error_message_street; ?> </div>
+                <div class="alert alert-success" id="street-alert" role="alert" <?php echo $style_success_street?> > <?php echo $success_message_street; ?> </div>
+                    <label for="street">Street:</label>
+                    <input type="text" name="street" id="street" class="form-control" value="<?php echo $street; ?>" required>
+                </div>
+                <div class="form-group col-md-6">
+                <div class="alert alert-danger" id="streetnumber-alert" role="alert" <?php echo $style_warning_streetnumber?> > <?php echo $error_message_streetnumber; ?> </div>
+                <div class="alert alert-success" id="streetnumber-alert" role="alert" <?php echo $style_success_streetnumber?> > <?php echo $success_message_streetnumber; ?> </div>
+                    <label for="streetnumber">Street number:</label>
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo $streetnumber; ?>" required>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                <div class="alert alert-danger" id="city-alert" role="alert" <?php echo $style_warning_city?> > <?php echo $error_message_city; ?> </div>
+                <div class="alert alert-success" id="city-alert" role="alert" <?php echo $style_success_city?> > <?php echo $success_message_city; ?> </div>
+                    <label for="city">City:</label>
+                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $city; ?>" required>
+                </div>
+                <div class="form-group col-md-6">
+                <div class="alert alert-danger" id="zipcode-alert" role="alert" <?php echo $style_warning_zipcode?> > <?php echo $error_message_zipcode; ?> </div>
+                <div class="alert alert-success" id="zipcode-alert" role="alert" <?php echo $style_success_zipcode?> > <?php echo $success_message_zipcode; ?> </div>
+                    <label for="zipcode">Zipcode</label>
+                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php echo $zipcode; ?>" required>
+                </div>
+            </div>
+        </fieldset>
+
+        <fieldset>
+            <legend>Products</legend>
+            <div class="alert alert-danger" id="products-alert" role="alert" <?php echo $style_warning_products?> > <?php echo $error_message_products; ?> </div>
+            <div class="alert alert-success" id="products-alert" role="alert" <?php echo $style_success_products?> > <?php echo $success_message_products; ?> </div>
+            <?php foreach ($products AS $i => $product): ?>
+                <label>
+                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
+                    &euro; <?php echo number_format($product['price'], 2) ?></label><br />
+            <?php endforeach; ?>
+        </fieldset>
+
+        <button type="submit" class="btn btn-primary">Order!</button>
+    </form>
+
+    <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food and drinks.</footer>
+</div>
+
+<style>
+    footer {
+        text-align: center;
+    }
+</style>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,281 @@
+<?php
+//this line makes PHP behave in a more strict way
+declare(strict_types=1);
+
+//we are going to use session variables so we need to enable sessions
+session_start();
+
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+whatIsHappening();
+
+//your products with their price.
+$products = [
+    ['name' => 'Club Ham', 'price' => 3.20],
+    ['name' => 'Club Cheese', 'price' => 3],
+    ['name' => 'Club Cheese & Ham', 'price' => 4],
+    ['name' => 'Club Chicken', 'price' => 4],
+    ['name' => 'Club Salmon', 'price' => 5]
+];
+
+$products = [
+    ['name' => 'Cola', 'price' => 2],
+    ['name' => 'Fanta', 'price' => 2],
+    ['name' => 'Sprite', 'price' => 2],
+    ['name' => 'Ice-tea', 'price' => 3],
+];
+
+$totalValue = 0;
+
+// Clean input data
+function sanitizeData($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// Form validation
+$email = (isset($_POST["email"])) ? sanitizeData($_POST['email']) : '';
+
+// Address SESSION setting
+$street = (isset($_SESSION["stored_street"])) ? $_SESSION ['stored_street'] : '';
+$streetnumber = (isset($_SESSION["stored_streetnumber"])) ? $_SESSION ['stored_streetnumber'] : '';
+$city = (isset($_SESSION["stored_city"])) ? $_SESSION ['stored_city'] : '';
+$zipcode = (isset($_SESSION["stored_zipcode"])) ? $_SESSION ['stored_zipcode'] : '';
+
+if (isset($_POST["street"])) {
+    $street = sanitizeData($_POST["street"]); 
+}else {
+    $street = "";
+}
+
+if (isset($_POST["streetnumber"])) {
+    $streetnumber = sanitizeData($_POST["streetnumber"]); 
+}else {
+    $streetnumber = "";
+}
+
+if (isset($_POST["city"])) {
+    $city = sanitizeData($_POST["city"]); 
+}else {
+    $city = "";
+}
+
+if (isset($_POST["zipcode"])) {
+    $zipcode = sanitizeData($_POST["zipcode"]); 
+}else {
+    $zipcode = "";
+}
+
+// Error Alert Message
+$error_message_email = "";
+$error_message_street = "";
+$error_message_streetnumber = "";
+$error_message_city = "";
+$error_message_zipcode = "";
+$error_message_products = "";
+
+// Success Alert Message
+$success_message_email = "";
+$success_message_street = "";
+$success_message_streetnumber = "";
+$success_message_city = "";
+$success_message_zipcode = "";
+$success_message_products = "";
+
+// Warning style alert
+$style_warning_email = "";
+$style_warning_street = "";
+$style_warning_streetnumber = "";
+$style_warning_city = "";
+$style_warning_zipcode = "";
+$style_warning_products ="";
+
+// Success style alert
+$style_success_email = "";
+$style_success_street = "";
+$style_success_streetnumber = "";
+$style_success_city = "";
+$style_success_zipcode = "";
+$style_success_products ="";
+
+// Validate true or false
+$return_email = "";
+$return_street = "";
+$return_streetnumber = "";
+$return_city = "";
+$return_zipcode = "";
+$return_products ="";
+
+/* $style_warning_show= "";
+$style_warning = array();
+array_push($style_warning, $_POST['email'], $_POST["street"], $_POST["streetnumber"], $_POST["city"], $_POST["zipcode"]);
+print_r($style_warning);
+
+foreach ( $style_warning as $style_warning_message ) {
+  if ($style = false) {   
+    $style_warning_show= $style_success_zipcode = "style='display:none;'";
+  } else {
+    $style = "";
+  }
+} */
+
+// Form invalid
+$style_success = "";
+if (empty($_POST['email']) || empty($_POST["street"]) || empty($_POST["streetnumber"]) || empty($_POST["city"]) || empty($_POST["zipcode"])|| empty($_POST["products"])) {
+    $style_success = "style='display:none;'";
+};
+
+// Form empty
+if (empty($_POST['email']) && empty($_POST["street"]) && empty($_POST["streetnumber"]) && empty($_POST["city"]) && empty($_POST["zipcode"]) && empty($_POST["products"])) {
+    $style_warning_email =  "style='display:none;'";
+    $style_success_email = "style='display:none;'";
+    $style_warning_street =  "style='display:none;'";
+    $style_success_street = "style='display:none;'";
+    $style_warning_streetnumber =  "style='display:none;'";
+    $style_success_streetnumber = "style='display:none;'";
+    $style_warning_city =  "style='display:none;'";
+    $style_success_city = "style='display:none;'";
+    $style_warning_zipcode =  "style='display:none;'";
+    $style_success_zipcode = "style='display:none;'";
+
+};
+
+//Form filled
+if (isset($_POST['email']) && isset($_POST["street"]) && isset($_POST["streetnumber"]) && isset($_POST["city"]) && isset($_POST["zipcode"])) {
+
+    // Validate email
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+     //$_SESSION["stored_email"] = $email;
+     $success_message_email .= "Valid";
+     $style_warning_email = "style='display:none;'";
+     $return_email = true;
+    } else {
+     $error_message_email .= 'The email you entered is invalid.<br>';
+     $style_success_email = "style='display:none;'";
+     $return_email = true;
+     //$delete_email .= "unset("."$_SESSION"."['stored_email'])";
+     //unset($_SESSION['stored_email']);
+     //session_destroy();
+    }
+
+    // Validate address
+    $string_exp = "/^[A-Za-z .'-]+$/";
+    $numbers_exp = "/^[0-9]+$/";
+    $house_numbers_exp= "/^\d+[a-zA-Z]*$/";
+
+    if (!preg_match($string_exp, $street)) {
+        $error_message_street .= 'The street you entered is invalid.<br>';
+        $style_success_street = "style='display:none;'";
+        $return_street = false;
+        unset($_SESSION["stored_street"]);
+    } else {
+        $style_warning_street = "style='display:none;'";
+        $success_message_street .= "Valid";
+        $return_street = true;
+        $_SESSION["stored_street"] = $street;
+    }
+
+    if (!preg_match($house_numbers_exp, $streetnumber)) {
+        $error_message_streetnumber .= 'The streetnumber you entered is invalid.<br>';
+        $style_success_streetnumber = "style='display:none;'";
+        $return_streenumber = false;
+        unset($_SESSION["streetnumber"]);
+    }
+    else {
+        $style_warning_streetnumber = "style='display:none;'";
+        $success_message_streetnumber .= "Valid";
+        $return_streetnumber = true;
+        $_SESSION["stored_streetnumber"] = $streetnumber;
+    }
+    
+    if (!preg_match($string_exp, $city)) {
+        $error_message_city .= 'The city you entered is invalid.<br>';
+        $style_success_city = "style='display:none;'";
+        $return_city = false;
+        unset($_SESSION["stored_city"]);
+    }
+    else {
+       $style_warning_city = "style='display:none;'";
+       $success_message_city .= "Valid";
+       $return_city = true;
+       $_SESSION["stored_city"] = $city;
+    }
+
+    if (!preg_match($numbers_exp, $zipcode)) {
+        $error_message_zipcode .= 'The zipcode you entered is invalid.<br>';
+        $style_success_zipcode = "style='display:none;'";
+        $return_zipcode = false;
+        unset($_SESSION["stored_zipcode"]);
+    }
+    else {
+       $style_warning_zipcode= "style='display:none;'";
+       $success_message_zipcode .= "Valid";
+       $return_zipcode = true;
+       $_SESSION["stored_zipcode"] = $zipcode;
+    }
+
+};
+
+// Product choice validate 
+ if(isset($_POST['products']) === 1) {
+        $return_products == false; 
+        $style_success_products = "style='display:none;'";
+        $error_message_products .= 'Please make a choice.<br>';
+    } else {
+        $return_products == true;
+        $style_warning_products = "style='display:none;'";
+        $success_message_products .= "Valid Choice!";
+    };
+
+// Message for user if form is validated
+ $thankyou_message = "";
+
+ if ($return_email == true && $return_street == true && $return_streetnumber == true && $return_city == true && $return_zipcode == true && $return_products == true)  
+ {
+    $thankyou_message = 'Your order is submitted, thank you';
+    $style_warning_email = "style='display:none;'";
+    $style_warning_street = "style='display:none;'";
+    $style_warning_streetnumber = "style='display:none;'";
+    $style_warning_city = "style='display:none;'";
+    $style_warning_zipcode = "style='display:none;'";
+
+    $style_success_email = "style='display:none;'";
+    $style_success_street = "style='display:none;'";
+    $style_success_streetnumber ="style='display:none;'";
+    $style_success_city = "style='display:none;'";
+    $style_success_zipcode = "style='display:none;'";
+}; 
+
+// Form invalidated
+if ($return_email == false || $return_street == false || $return_streetnumber == false || $return_city == false || $return_zipcode == false || $return_products == false)  
+ {   $style_success = "style='display:none;'";
+ };
+
+ // Email order to yourself
+    $email_to = "nicol.saha@gmail.com";
+    $email_subject = "New form submissions";
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    @mail($email_to, $email_subject, $email_message);
+
+    
+require 'form-view.php'; 
